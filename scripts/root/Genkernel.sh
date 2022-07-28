a8dc34436d956b4e294506d445d61fc5ee6ccb8c
hoeding <hoeding@gmail.com>
2022-07-28 03:45:46 -0600

    gitignore

--- scripts/root/Genkernel.sh
+++ scripts/root/Genkernel.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
 cd /usr/src/linux
-genkernel all --makeopts="-j16 -l12" --loglevel=5 --menuconfig --no-save-config --oldconfig --kernel-config=/usr/src/linux/.config --config=/etc/genkernel.conf
+genkernel all --makeopts="-j5 -l4" --loglevel=5 --menuconfig --no-save-config --oldconfig --kernel-config=/usr/src/linux/.config --config=/etc/genkernel.conf
